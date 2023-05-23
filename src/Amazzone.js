e80901712494fc920fc3afd2142fe2756ad8ceb9
Riccardo <rikio.masio@gmail.com>
2023-05-23 22:18:28 +0200

    Modifiche post consegna

--- src/Amazzone.js
+++ src/Amazzone.js
@@ -3,19 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Outlet, Link } from "react-router-dom"
 import { useState, useEffect } from "react"
 import imagine from "./pages/Assets/Immages/Amazon_logo.svg.png";
-import imagine2 from "./pages/Assets/Immages/shopping-cart-free-png.webp";
+import imagine2 from "./pages/Assets/Immages/pngwing.com.png";
 import Page2 from "./pages/Le novità più interessanti";
 import Page3 from "./pages/I prodotti del momento";
 import Page4 from "./pages/I più desiderati";
 import Page5 from "./pages/I più regalati";
-
-
-
-
-
-
-
-
 
 function Main() {
   const [products, setProducts] = useState([])
@@ -23,8 +15,9 @@ function Main() {
       async function fetchProducts () {
           let res = await fetch ("https://dummyjson.com/products")
           let json = await res.json ()
+          let products = json.products
           console.log(json);
-          setProducts (json)
+          setProducts (products)
   
       }
       fetchProducts ()
@@ -38,8 +31,8 @@ function Main() {
         </div>
 
         <div className="search-bar"> 
-        <label for="site-search"> <b> Ricerca prodotti:</b></label>
-          <input type="search" id="site-search" name="q"/>
+        <label htmlFor="site-search"> <b> Ricerca prodotti:</b></label>
+          <input type="search" id="site-search" name="q" />
 
           <button type="button">Search</button>
         </div>
@@ -53,9 +46,12 @@ function Main() {
       </header>
 
       <nav className='nav-bar'>
-        <ul>
+        <ul>          
           <li>
-            <Link to="Page2">Le novità più interessanti</Link>
+            <Link to="/">Bestseller di Amazon</Link>
+          </li>
+          <li>
+            <Link to="/Page2">Le novità più interessanti</Link>
           </li>
           <li>
             <Link to="Page3">I prodotti del momento</Link>
@@ -64,38 +60,35 @@ function Main() {
             <Link to="Page4">I più desiderati</Link>
           </li>
           <li>
-            <Link to="Page4">I più regalati</Link>
+            <Link to="Page5">I più regalati</Link>
           </li>
+
+
         </ul>
-        
-      </nav>
-
-      <body>
-
         <h2>Bestseller di Amazon</h2>
         <h4>I nostri prodotti più popolari in base alle vendite. Aggiornamento frequente.</h4>
+      </nav>
 
-        {/* {products.map((product) => (
+      <main>
+
+        {products.map((product) => (
         <div key={product.id}>
-        <h3>{product.titolo}</h3>
-        <p>{product.prezzo}</p>
-        <p>{product.brand}</p>
-        <img src={product.immagine} alt={product.titolo} />
+        <h3>{product.title }</h3>
+        <p id='price'>{product.price}</p>
+        <p id='brand'>{product.brand}</p>
+        <img src={product.images[0]} alt={product.title} />
+
+          <button id='add' type="button">Aggiungi al carrello</button>
+          <button id='buy' type="button">Acquista ora</button>
+
+
+
         </div>
-        ))} */}
-
-        {/* Qui ho provato a selezionare prezzo, brand, id, titolo e immgine dei prodotti solo che non funziona e mi esce "products.map is not a function..." */}
+        ))}
 
 
 
-        
-
-        
-        
-      </body>
-
-      
-
+      </main>
     </>
   )
 }
@@ -106,14 +99,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}>
-        <Route path="Le novità più interessanti" element={<Page2 />} />
-        <Route path="I prodotti del momento" element={<Page3 />} />
-        <Route path="I più desiderati" element={<Page4 />} />
-        <Route path="I più regalati" element={<Page5 />} />
-
-
-        </Route>
+        <Route path="/" element={<Main />}/>
+        <Route path="/Page2" element={<Page2 />} />
+        <Route path="/Page3" element={<Page3 />} />
+        <Route path="/Page4" element={<Page4 />} />
+        <Route path="/Page5" element={<Page5 />} />
       </Routes>
     </BrowserRouter>
   )
